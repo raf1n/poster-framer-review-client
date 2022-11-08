062e011fd5c8f08053f5f5522578db08cd6c8b79
raf1n <rafinc10@gmail.com>
2022-11-08 12:09:34 +0600

    router-setup-and-component-structure-added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import { router } from "./Routes/Router";
 
 function App() {
   return (
     <div>
-      <h1 className="text-5xl">Hello</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
