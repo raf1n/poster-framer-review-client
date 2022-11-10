91e2ab52d1de52ec47690eec4a573fb8d0196af3
raf1n <rafinc10@gmail.com>
2022-11-10 23:28:02 +0600

    full-image-view-added-on-service-details-page

--- src/components/ServiceDetail/ServiceDetail.js
+++ src/components/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,16 @@
 import React from "react";
-
+import { PhotoProvider, PhotoView } from "react-photo-view";
 const ServiceDetail = ({ service }) => {
   const { name, img, price, description } = service;
   return (
     <section className="flex justify-center my-6 mx-16">
       <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
-        <img src={img} alt="" className="h-80 bg-gray-500 aspect-video" />
+        <PhotoProvider>
+          <PhotoView src={img}>
+            <img src={img} alt="" className="h-80 bg-gray-500 aspect-video" />
+          </PhotoView>
+        </PhotoProvider>
+
         <div className="flex flex-col justify-center flex-1 p-6 bg-gray-50">
           <h3 className="text-3xl font-bold">{name}</h3>
           <p className="my-6 text-gray-600">{description}</p>
